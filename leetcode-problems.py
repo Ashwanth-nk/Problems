6fd46d12ddb6752a848777a512ee43ea99b70194
ashwanthnk5@gmail.com <kasisnk@Kasis-MacBook-Pro.local>
2026-05-29 18:10:52 +0530

    3 Problems (29 May 2026)

--- leetcode-problems.py
+++ leetcode-problems.py
@@ -1,7 +1,7 @@
-
 '''
+# Hard
 # 3093. Longest Common Suffix (Hard)
-# 800+ Testcases ran
+# 800+ Testcases ran (Dint pass 9)
 # Error: Time Limit Executed
 wordsContainer = ["abcd","bcd","xbcd"]
 wordsQuery = ["cd","bcd","xyz"]
@@ -32,6 +32,71 @@ for i in wordsQuery:
         ans.append(wordsContainer.index(x[0][0]))
     else:
         ans.append(wordsContainer.index(sorted(wordsContainer, key=len)[0]))
-
 print(ans)
+
+
+// Easy
+// 58. Length of Last Word
+// 100% Runtime, 45% Memory
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    let u = s.trim().split(/\s+/);
+    return u[u.length-1].length;
+};
+
+
+// Easy
+// 28. Find the Index of the First Occurrence in a String
+// 100% Runtime, 67% Memory
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    if (haystack.length < needle.length){
+        return -1
+    }
+    let i = 0;
+    let j = needle.length;
+    let c = 0;
+    while (j<haystack.length+1){
+        if (haystack.slice(i,j) === needle) {
+            return c
+        }
+        i++
+        j++
+        c++
+    }
+    return -1
+};
+
+
+// Easy
+// 3300. Minimum Element After Replacement With Digit Sum
+// 76% Runtime, 63% Memory
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minElement = function(nums) {
+    let least = null;
+    for (let i = 0; i<nums.length; i++) {
+        let ans = 0;
+        a = nums[i];
+        while (a>0){
+            ans+=(a%10)
+            a = Math.floor(a/10)
+        }
+        if (least === null){
+            least = ans
+        } else if (ans < least){
+            least = ans
+        }
+    }
+    return least;
+};
 '''
